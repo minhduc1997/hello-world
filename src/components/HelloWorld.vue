fdb18df8ca95fdfbb4ab1714eab8267b0132c7a3
minhduc1997 <minhd2474@gmail.com>
2022-03-02 11:11:24 +0700

    Duc commit: commit qrcode

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,78 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <p class="decode-result">Last result: <b>{{ result }}</b></p>
+
+
+    <qrcode-stream :camera="camera" @decode="onDecode" @init="onInit">
+      <div v-show="showScanConfirmation" class="scan-confirmation">
+        <img src="favicon.ico" alt="Checkmark" width="128px" />
+      </div>
+    </qrcode-stream>
   </div>
 </template>
 
 <script>
+import { QrcodeStream } from 'vue-qrcode-reader'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+
+  components: { QrcodeStream },
+
+  data () {
+    return {
+      camera: 'auto',
+      result: null,
+      showScanConfirmation: false
+    }
+  },
+
+  methods: {
+
+    async onInit (promise) {
+      try {
+        await promise
+      } catch (e) {
+        console.error(e)
+      } finally {
+        this.showScanConfirmation = this.camera === "off"
+      }
+    },
+
+    async onDecode (content) {
+      this.result = content
+
+      this.pause()
+      await this.timeout(500)
+      this.unpause()
+    },
+
+    unpause () {
+      this.camera = 'auto'
+    },
+
+    pause () {
+      this.camera = 'off'
+    },
+
+    timeout (ms) {
+      return new Promise(resolve => {
+        window.setTimeout(resolve, ms)
+      })
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.scan-confirmation {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(255, 255, 255, .8);
+
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
 }
 </style>
